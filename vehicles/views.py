4b15af6fd83925fdf9186b5fb6d588a64e73d195
ViniCrK <vinicrk@outlook.com>
2024-05-07 12:29:12 -0300

    Add list and create views

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,3 +1,23 @@
+from typing import Any
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Vehicle
 
-# Create your views here.
+
+class ListVehiclesView(ListView):
+    model = Vehicle
+    template_name = 'vehicles/list_vehicles.html'
+    context_object_name = 'vehicles'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context.update({
+            'site_title': 'Veículos'
+        })
+
+        return context
+
+
+class DetailVehicleView(DetailView):
+    ...
