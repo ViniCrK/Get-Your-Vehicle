f9bae5ec6402be3192299e12f30fa38c50dd4030
ViniCrK <vinicrk@outlook.com>
2024-05-07 12:31:36 -0300

    Set models on admin page

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from vehicles import models
 
-# Register your models here.
+
+@admin.register(models.Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    ordering = ('id',)
+
+
+@admin.register(models.Color)
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    ordering = ('id',)
+
+
+@admin.register(models.Type)
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    ordering = ('id',)
+
+
+@admin.register(models.Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type', 'brand', 'model',)
+    list_per_page = 10
+    ordering = ('-id',)
