a4280e9f1cf7a58a5a1ac878921e2aaf9539c15e
ViniCrK <vinicrk@outlook.com>
2024-05-07 16:39:34 -0300

    Change meta options | Add field status | Alter all fields

--- vehicles/models/vehicle.py
+++ vehicles/models/vehicle.py
@@ -5,11 +5,35 @@ from .color import Color
 
 
 class Vehicle(models.Model):
-    type = models.ForeignKey(Type, on_delete=models.SET_NULL, null=True)
-    brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True)
-    model = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='images/%Y/%m/', null=True)
-    color = models.ForeignKey(Color, on_delete=models.SET_NULL, null=True)
-    manufacture_year = models.CharField(max_length=10)
-    price = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
-    wheels_number = models.IntegerField(blank=True, null=True)
+    class Meta:
+        verbose_name = 'Veículo'
+        verbose_name_plural = 'Veículos'
+
+    VEHICLE_STATUS = {
+        'ER': 'Em Revisão',
+        'D': 'Disponível',
+        'I': 'Indisponível',
+    }
+
+    type = models.ForeignKey(
+        Type, on_delete=models.SET_NULL, null=True, verbose_name='Tipo')
+    brand = models.ForeignKey(
+        Brand, on_delete=models.SET_NULL, null=True, verbose_name='Marca')
+    model = models.CharField(max_length=50, verbose_name='Modelo')
+    image = models.ImageField(
+        upload_to='images/%Y/%m/', null=True, verbose_name='Imagem')
+    color = models.ForeignKey(
+        Color, on_delete=models.SET_NULL, null=True, verbose_name='Cor')
+    manufacture_year = models.CharField(
+        max_length=10, verbose_name='Ano de Fabricação')
+    status = models.CharField(max_length=2, null=True,
+                              choices=VEHICLE_STATUS, verbose_name='Status')
+    price = models.DecimalField(
+        default=0.00, max_digits=9, decimal_places=2, verbose_name='Preço')
+    have_wheels = models.BooleanField(
+        default=False, verbose_name='Possui Rodas')
+    wheels_number = models.IntegerField(
+        blank=True, null=True, verbose_name='Quantidade de Rodas')
+
+    def __str__(self) -> str:
+        return f'{self.model}'
