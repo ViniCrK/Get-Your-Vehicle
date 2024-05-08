2a7180207100b8b35ddb4c6582fdebd11b7e2e87
ViniCrK <vinicrk@outlook.com>
2024-05-08 14:34:51 -0300

    Remove model attribute and add a queryset for ListVehiclesView

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,13 +1,12 @@
-from typing import Any
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Vehicle
 
 
 class ListVehiclesView(ListView):
-    model = Vehicle
-    template_name = 'vehicles/list_vehicles.html'
+    template_name = 'vehicles/list.html'
     context_object_name = 'vehicles'
+    queryset = Vehicle.objects.filter(status='D')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
