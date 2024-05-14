11a38c4ffef1db347ef54707a5c2856dce62096d
ViniCrK <vinicrk@outlook.com>
2024-05-14 08:38:16 -0300

    Add slug parameter to path for detail route

--- vehicles/urls.py
+++ vehicles/urls.py
@@ -5,5 +5,5 @@ app_name = 'vehicles'
 
 urlpatterns = [
     path('', views.ListVehiclesView.as_view(), name='list'),
-    path('<int:id>/', views.DetailVehicleView.as_view(), name='detail'),
+    path('<slug:slug>/', views.DetailVehicleView.as_view(), name='detail'),
 ]
