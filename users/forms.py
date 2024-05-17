7387a8265730972d1f44d71005d92f717f85e781
ViniCrK <vinicrk@outlook.com>
2024-05-17 09:15:19 -0300

    Change some form fields

--- users/forms.py
+++ users/forms.py
@@ -4,11 +4,12 @@ from django.contrib.auth.models import User
 
 
 class UserRegisterForm(UserCreationForm):
+    sobrenome = forms.CharField(max_length=100, required=True)
     cpf = forms.CharField(
         max_length=14, help_text='Insira seu CPF sem ponto(.) ou hífen(-)')
-    phone = forms.CharField(max_length=14)
+    celular = forms.CharField(max_length=14)
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2',)
+        fields = ('first_name', 'sobrenome', 'username', 'email', 'password1',)
