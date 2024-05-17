28d03de0c6baa3f2550dc53e46b226fc5ab0fa24
ViniCrK <vinicrk@outlook.com>
2024-05-17 09:14:51 -0300

    Add success_url and update context

--- users/views.py
+++ users/views.py
@@ -8,3 +8,13 @@ class UserRegisterView(CreateView):
     model = User
     form_class = UserRegisterForm
     template_name = 'users/register.html'
+    success_url = 'vehicles/list'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context.update({
+            'site_title': "Cadastro de Usuário"
+        })
+
+        return context
