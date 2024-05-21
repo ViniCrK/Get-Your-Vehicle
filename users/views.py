e46b3135c36df9a245ef41c05bd02c6890f19da3
ViniCrK <vinicrk@outlook.com>
2024-05-21 14:23:26 -0300

    Create login view

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import View, CreateView
+from django.contrib.auth.views import LoginView
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from .forms import UserRegisterForm
 
 
@@ -8,13 +10,28 @@ class UserRegisterView(CreateView):
     model = User
     form_class = UserRegisterForm
     template_name = 'users/register.html'
-    success_url = 'vehicles/list'
+    success_url = reverse_lazy('users:login')
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(**kwargs)
 
         context.update({
             'site_title': "Cadastro de Usuário"
+        })
+
+        return context
+
+
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('vehicles/list')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context.update({
+            'site_title': "Autenticação de Usuário"
         })
 
         return context
