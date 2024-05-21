674ee8554b2deb089f9a65facd27d15454346cd6
ViniCrK <vinicrk@outlook.com>
2024-05-21 14:22:01 -0300

    Add path for login and logout views

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from . import views
 
@@ -5,4 +6,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('register/', views.UserRegisterView.as_view(), name='register'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
