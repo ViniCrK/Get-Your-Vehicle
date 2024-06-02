7bdb790bcbba1cd17c1e500b3244493adffc3d03
ViniCrK <vinicrk@outlook.com>
2024-06-02 16:53:55 -0300

    Add route to RentVehicleView

--- vehicles/urls.py
+++ vehicles/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.ListVehiclesView.as_view(), name='list'),
     path('search/', views.SearchListView.as_view(), name='search'),
     path('<slug:slug>/', views.DetailVehicleView.as_view(), name='detail'),
+    path('<slug:slug>/rent/', views.RentVehicleView.as_view(), name='rent'),
 ]
